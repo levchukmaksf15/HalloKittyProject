27c82b184c3113d473f5cf929a004c4da569735d
Levchuk Maksym <levchukmaksf@gmail.com>
2020-03-14 13:32:13 +0200

    First commit

---
+++ app/src/main/java/com/non_zero/hallokitty/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.non_zero.hallokitty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
