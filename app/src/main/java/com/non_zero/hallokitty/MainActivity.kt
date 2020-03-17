1ab636e81251c2f6d989c9b0d99792fb4e3479bb
Levchuk Maksym <levchukmaksf@gmail.com>
2020-03-17 17:57:49 +0200

    Created imageButton, which change textView with text from editText

--- app/src/main/java/com/non_zero/hallokitty/MainActivity.kt
+++ app/src/main/java/com/non_zero/hallokitty/MainActivity.kt
@@ -2,11 +2,24 @@ package com.non_zero.hallokitty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mHelloTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageButton.setOnClickListener{
+            if (editText.text.isEmpty()){
+                textView.text = "Hello Kitty"
+            } else {
+                textView.text = "Hello, " + editText.text
+            }
+        }
     }
 }
